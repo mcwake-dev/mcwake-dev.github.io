89ec5acd1b27c7e13b92325f7c1511797bfe940c
Matthew C Wake <mcwake-dev@outlook.com>
2021-09-30 14:29:41 +0100

    Basic implementation of blog system created.

--- components/BlogCard/index.js
+++ components/BlogCard/index.js
@@ -2,11 +2,15 @@ import Link from 'next/link';
 import styles from './BlogCard.module.css';
 
 export default function BlogCard({entry}) {
-    const {title, excerpt} = entry.frontmatter;
+    const {title, excerpt, date_published} = entry.frontmatter;
 
     return (
         <article className={styles.blogCard}>
-            <header>{title}</header>
+            <header>
+                {title}
+                <br />
+                <em>Published {date_published}</em>
+            </header>
             <p>
                 {excerpt}
             </p>
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,23 +1,29 @@
-import path from 'path';
-import Disqus from 'disqus-react';
+import marked from 'marked';
+import Link from 'next/link';
 
 import styles from "../../components/BlogEntry.module.css";
 import { generateStaticPaths, importMarkdownFileContent } from '../../shared/libs/importMarkdown';
-import { BLOG_DISQUS_HOST, BLOG_DISQUS_SHORTNAME, BLOG_MARKDOWN_PATH } from "../../constants/blog";
+import { BLOG_MARKDOWN_PATH } from '../../constants/blog'
 
 export default function BlogEntry({ frontmatter: { title, excerpt, tech_used }, content }) {
-    const disqusConfig = {
-        url: path.join(BLOG_DISQUS_HOST, 'bob'),
-        identifier: path.join(BLOG_DISQUS_HOST, 'bob'),
-        title
-    }
-    
     return (
         <div id={styles.container}>
-            <Disqus.DiscussionEmbed
-                shortname='BLOG_DISQUS_SHORTNAME'
-                config={disqusConfig}
-            />
+        <section id={styles.banner}>
+            <Link href="/blog">
+                <a>&lt;-- Back to All Entries</a>
+            </Link>
+            <header>{title}</header>
+            <p>
+                <em>{excerpt}</em>
+            </p>
+        </section>
+        <section id={styles.content} dangerouslySetInnerHTML={{__html: marked(content, {breaks: true})}}>
+
+        </section>
+        <br />
+        <Link href="/blog">
+            <a>&lt;-- Back to All Entries</a>
+        </Link>
         </div>
     );
 }
